4e712039d98f2793b5aa4beb3754b40bae38f835
kin4496 <48973279+kin4496@users.noreply.github.com>
2025-04-10 20:00:26 +0900

    Title: 바이러스 검사, Time: 353ms, Memory: 4MB, Status: Wrong Answer - Codetree

---
+++ 250410/바이러스 검사/virus-detector.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int n;
+vector<int> rests;
+int leader, follower;
+
+int solve(int);
+
+int main() {
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        int cust;
+        cin>>cust;
+        rests.push_back(cust);
+    }
+
+    cin>>leader>>follower;
+
+    int ans = 0;
+    for(int i=0;i<n;i++)
+    {
+        ans += solve(rests[i]);
+    }
+
+    cout <<ans<<endl;
+    return 0;
+}
+
+int solve(int cust)
+{
+    if(cust <= leader) return 1;
+
+    cust -= leader;
+
+    return ((cust + follower - 1) / follower) + 1;
+}
